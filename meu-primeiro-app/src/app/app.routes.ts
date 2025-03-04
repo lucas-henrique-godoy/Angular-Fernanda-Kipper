4b9f9407e98c3cfd7d8867a397fc854c6cc32a52
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-03 23:41:21 -0300

    Apaguei comentarios

--- meu-primeiro-app/src/app/app.routes.ts
+++ meu-primeiro-app/src/app/app.routes.ts
@@ -3,8 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
+        path: "",
+        component: HomeComponent
+    },
+    {
         path: "home",
         component: HomeComponent
-    }
+    },
 
 ];
